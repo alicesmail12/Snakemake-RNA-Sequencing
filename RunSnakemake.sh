43c4f61473811c5ab5f60a3498b37e11ffa2805b
Alice Smail <114085896+alicesmail12@users.noreply.github.com>
2025-05-08 15:28:53 +0100

    Update RunSnakemake.sh

--- RunSnakemake.sh
+++ RunSnakemake.sh
@@ -4,11 +4,13 @@
 #SBATCH --mem=64G
 #SBATCH -c 12
 
+# Create directories
 cd /Alice/RNAseq-Pipeline-Snakemake/
 if [ ! -d ./FastQC-Output/ ]; then mkdir -p ./FastQC-Output/; fi
 if [ ! -d ./HISAT2/ ]; then mkdir -p ./HISAT2/; fi
 if [ ! -d ./FeatureCounts/ ]; then mkdir -p ./FeatureCounts/; fi
 
+# Load required modules
 module load R
 module load Graphviz
 module load snakemake
@@ -18,5 +20,8 @@ module load HISAT2/2.1.0-foss-2017b gompi/2017b
 module load SAMtools/1.11-GCC-10.2.0
 module load Subread/2.0.3-GCC-11.2.0
 
+# Snakemake DAG
 snakemake -c12 --dag FeatureCounts/GH1_S1_Counts.txt FastQC-Output/GH1_S1_R1_001_fastqc.html | dot -Tsvg > dag.svg
+
+# Run Snakefile using 12 cores
 snakemake -c12
