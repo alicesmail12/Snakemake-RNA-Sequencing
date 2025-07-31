ae090578c9fbe2611e5f9b78e1ef56475b7150ae
Alice Smail <114085896+alicesmail12@users.noreply.github.com>
2025-07-31 16:04:45 +0100

    Update RunSnakemake.sh

--- RunSnakemake.sh
+++ RunSnakemake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --output=Snakemake.out
-#SBATCH -p bigmem
+#SBATCH -p {node}
 #SBATCH --mem=64G
 #SBATCH -c 12
 
@@ -11,17 +11,11 @@ if [ ! -d ./HISAT2/ ]; then mkdir -p ./HISAT2/; fi
 if [ ! -d ./FeatureCounts/ ]; then mkdir -p ./FeatureCounts/; fi
 
 # Load required modules
-module load R
-module load Graphviz
-module load snakemake
-snakemake --unlock
-module load FastQC
-module load HISAT2/2.1.0-foss-2017b gompi/2017b
-module load SAMtools/1.11-GCC-10.2.0
-module load Subread/2.0.3-GCC-11.2.0
+module load R Graphviz snakemake
+module load FastQC HISAT2/2.1.0-foss-2017b gompi/2017b SAMtools/1.11-GCC-10.2.0 Subread/2.0.3-GCC-11.2.0
 
 # Snakemake DAG
-snakemake -c12 --dag /Alice/RNAseq-Pipeline-Snakemake/FeatureCounts/GH1_S1_Counts.txt /Alice/RNAseq-Pipeline-Snakemake/FastQC-Output/GH1_S1_R1_001_fastqc.html | dot -Tsvg > dag.svg
+snakemake -c12 --dag ./FeatureCounts/Sample1_Counts.txt ./FastQC-Output/Sample1_fastqc.html | dot -Tsvg > dag.svg
 
 # Run Snakefile using 12 cores
 snakemake -c12
